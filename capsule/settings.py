14a05a43c56b7497ac363eba82d5b1d6ef3948e9
alaingilbert <alain.gilbert.15@gmail.com>
2011-09-16 16:10:46 -0400

    Fix admin static path

--- capsule/settings.py
+++ capsule/settings.py
@@ -61,7 +61,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '_)9pzh134l_thoujjam(@9c4a6me6&2!yz2nmf4fh4gwf9ayb'
